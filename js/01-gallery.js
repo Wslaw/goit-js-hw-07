0cdb6153b8314fec06110da24905fc4f59afd1ea
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-10-26 17:08:34 +0300

    initLibrary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        console.log("ENTER ESCAPE");
+    }
+})
+
+
+
+
+
 console.log(galleryItems);
