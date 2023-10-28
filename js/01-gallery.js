7ceaaf4fae290e4925e77463528dea17abb3caf7
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-10-28 20:01:17 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,44 +2,29 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryContainer = document.querySelector(".gallery");
-galleryContainer.addEventListener("click", onImgClick);
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
 // console.log(itemsMarkup);
+// console.log(galleryItems);
+
+galleryContainer.addEventListener("click", onImgClick);
 galleryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
-console.log(galleryItems);
+
 function createGalleryItemsMarkup(items) {
-    return items.map(({ preview, original, description }) => {
-        return `<li class = "gallery__item">
-        <a class = "gallery__link" href ="${original}">
-        <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}" />
+  return items
+    .map(
+      (item) =>
+        `<li class = "gallery__item">
+    <a class = "gallery__link" href ="${item.original}">
+    <img
+    class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}" />
         </a>
-        </li>`;
-    })
-    .join("");
-}
-
-document.querySelector("img").onclick = () => {
-  basicLightbox.create(
-      `
-		<img width="1280" height="auto" src="">
-	`
+        </li>`
     )
-    .show();
-};
-
-function onImgClick(evt) {
-    evt.preventDefault();
-    
+    .join("");  
 }
 
-addEventListener("keydown", (event) => {
-  if (event.key === "Escape") {
-    console.log("ENTER ESCAPE");
-  }
-});
 
 
