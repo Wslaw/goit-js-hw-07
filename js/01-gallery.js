d13cd4612c2add3ab3ed4b72423534295590c521
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-10-28 22:48:04 +0300

    done-50%

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,27 @@
+
+const items = document.querySelectorAll("a");
+items.forEach(item => {
+  item.style.listStyleType = "none";
+  item.style.textDecoration = "none";
+  item.style.backgroundColor = "#97a5ff";
+  item.style.fontSize = "25px";
+  item.style.fontWeight = "500";
+  item.style.color = "blue";
+  item.style.display = "inline-block";
+  item.style.border = "2px solid #53f5ff";  
+  item.style.padding = "5px 30px";  
+item.style.borderRadius = "120px 100px 10px";
+});
+const container = document.querySelector("body");
+container.style.backgroundColor = "#70a8c2";
+
+
+
+
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+
 
 const galleryContainer = document.querySelector(".gallery");
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
@@ -10,11 +32,10 @@ galleryContainer.addEventListener("click", onImgClick);
 galleryContainer.insertAdjacentHTML("beforeend", itemsMarkup);
 let light;
 
-
 function createGalleryItemsMarkup(items) {
   return items
-  .map(
-    (item) =>
+    .map(
+      (item) =>
         `<li class = "gallery__item">
         <a class = "gallery__link" href ="${item.original}">
         <img
@@ -24,26 +45,22 @@ function createGalleryItemsMarkup(items) {
         alt="${item.description}" />
         </a>
         </li>`
-        )
-        .join("");  
-      }
-      
-      
-      
-      
-      function onImgClick(evt) {
-        evt.preventDefault();
-        document.addEventListener("keydown", onEscape);
-        if (evt.target.nodeName !== "IMG") return;
-        light = basicLightbox.create(`<img width="1280" height="auto" src="${evt.target.dataset.source}">`);
-        light.show();
-        onEscape(evt);
-        // console.log(evt.target.dataset.source);
-      }
-      
-      
-      function onEscape(evt) {
-        if (evt.key !== "Escape") return;
-        light.close();
-        document.removeEventListener("keydown", onEscape);
+    )
+    .join("");
+}
+
+function onImgClick(evt) {
+  evt.preventDefault();
+  document.addEventListener("keydown", onEscape);
+  if (evt.target.nodeName !== "IMG") return;
+  light = basicLightbox.create(`<img width="1280" height="auto" src="${evt.target.dataset.source}">`);
+  light.show();
+  onEscape(evt);
+  // console.log(evt.target.dataset.source);
+}
+
+function onEscape(evt) {
+  if (evt.key !== "Escape") return;
+  light.close();
+  document.removeEventListener("keydown", onEscape);
 }
